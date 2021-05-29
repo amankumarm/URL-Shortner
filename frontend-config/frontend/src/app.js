4a2da03fb17522198b09043cb59a492548124b6c
amankumarm <amankumarm441@gmail.com>
2021-05-29 16:18:52 +0530

    ready to deploy

--- frontend-config/frontend/src/app.js
+++ frontend-config/frontend/src/app.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react'
 import ReactDom from 'react-dom'
-// import Landing from './components/Landing'
-// import Main from './components/Main'
-// import "regenerator-runtime/runtime.js";
-// import {
-    // HashRouter as Router,
-    // Switch,
-    // Route,
-//   } from "react-router-dom";  
-import FouroFour from './components/Fourofour';
+import Landing from './components/Landing'
+import Main from './components/Main'
+import "regenerator-runtime/runtime.js";
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";  
+// import FouroFour from './components/Fourofour';
 function App() {
         return (
-            <FouroFour/>
-        //     // <Router>
-        //         {/* <Switch> */}
-        //             {/* <Route path="/thinn"> */}
-        //                 {/* <Main /> */}
-        //             {/* </Route> */}
-        //             {/* <Route path="/"> */}
-        //                 {/* <Landing /> */}
-        //             {/* </Route> */}
-        //     {/* </Switch> */}
-        // {/* </Router> */}
+            // <FouroFour/>
+            <Router>
+                <Switch>
+                    <Route path="/thinn">
+                        <Main />
+                    </Route>
+                    <Route path="/">
+                        <Landing />
+                    </Route>
+            </Switch>
+        </Router>
         )
 }
 ReactDom.render(<App/>,document.getElementById("root"));
